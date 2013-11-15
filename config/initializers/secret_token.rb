7a89f6db0a0470788f1186438193f1cd1e6d1bf8
Suzie Nguyen <stringstory@gmail.com>
2013-11-15 21:22:54 +1100

    pristine

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StoryMap::Application.config.secret_key_base = '0c03fe82d0283b9909814b3dcbe7372d97bbd5e6374e9da8d49bdff508fc28f7c6b90476d89a04aed8832bbcaca6f700764c0535589f4fab5eef4e94015b780a'
